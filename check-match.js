57ef819f43c77376f8536b24d7297298ffb4e8c9
Ceech Hsu <ceechhsu@gmail.com>
2026-03-04 14:07:57 -0800

    chore: remove temporary debug scripts breaking vercel build

--- check-match.js
+++
@@ -1,1 +0,0 @@
-console.log('Telling the user the exact URL.');
--- test-api-3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source .env.local
-URL="${NEXT_PUBLIC_SUPABASE_URL}"
-KEY="${SUPABASE_ACCESS_TOKEN:-$NEXT_PUBLIC_SUPABASE_ANON_KEY}"
-
-curl -s "$URL/rest/v1/sessions?match_id=eq.eefa585c-6aee-4b2b-a1f1-52bda70494e6&select=id,user_id,ends(id,end_index,shots(score))" \
-  -H "apikey: $KEY" \
-  -H "Authorization: Bearer $KEY"
